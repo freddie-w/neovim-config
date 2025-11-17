dad1061668634f7093d073b1bb482ad23e9b4687
Freddie Wellfair <freddie.wellfair@gmail.com>
2025-11-17 17:58:34 +0000

    add lazygit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,6 +17,7 @@ require('lazy').setup({
   -- require 'plugins.autopairs',
   require 'plugins.neo-tree',
   require 'plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'plugins.lazygit',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
